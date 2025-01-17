3fba1a59eb41317b1b1026f214690147f765c2eb
Dishma Gangulel <120539966+gangulel@users.noreply.github.com>
2025-01-17 21:26:01 +0530

    Arrays add

---
+++ Compound Data Types.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $colors=array("red"=>"ff0000","green"=>"00ff00","blue"=>"0000ff");
+        echo $colors["red"];
+    ?>
+</body>
+</html>
